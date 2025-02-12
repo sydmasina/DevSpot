d57d890f2736ad3981d7e4caca9e624cd10af2ac
sydmasina <masinasydwell@gmail.com>
2025-02-12 17:27:23 +0200

    add jobApllication model to dbset

--- DevSpot/Data/ApplicationDbContext.cs
+++ DevSpot/Data/ApplicationDbContext.cs
@@ -7,6 +7,9 @@ namespace DevSpot.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<JobPosting> JobPostings { get; set; }
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
