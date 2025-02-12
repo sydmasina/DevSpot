e4f78c299281f731250d49cf18a6c6cf3986daf9
sydmasina <masinasydwell@gmail.com>
2025-02-12 17:26:02 +0200

    Add job applications model

---
+++ DevSpot/Models/JobApplication.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevSpot.Models
+{
+    public class JobApplication
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+
+        [Required]
+        public int JobPostingId { get; set; }
+
+        [ForeignKey(nameof(JobPostingId))]
+        public JobPosting JobPosting { get; set; }
+
+        [Required]
+        public string Resume { get; set; }
+
+        [Required]
+        public string CoverLetter { get; set; }
+
+        [Required]
+        public DateTime ApplicationDate { get; set; } = DateTime.Now;
+    }
+}
