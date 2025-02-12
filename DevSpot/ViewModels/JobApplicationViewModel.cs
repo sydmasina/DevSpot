3417209bf1c335eba3d954e2830dbe372ad431ed
sydmasina <masinasydwell@gmail.com>
2025-02-12 17:27:01 +0200

    add jobApplication view

---
+++ DevSpot/ViewModels/JobApplicationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpot.ViewModels
+{
+    public class JobApplicationViewModel
+    {
+        [Required]
+        public int JobPostingId { get; set; }
+
+        [Required]
+        public string Resume { get; set; } = string.Empty;
+
+        public string? CoverLetter { get; set; } = string.Empty;
+    }
+}
