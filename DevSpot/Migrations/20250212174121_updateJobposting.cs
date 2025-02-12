302d5e7049a9525cede385a4b3b494b37a000d38
sydmasina <masinasydwell@gmail.com>
2025-02-12 20:27:51 +0200

    Update migrations

---
+++ DevSpot/Migrations/20250212174121_updateJobposting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevSpot.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateJobposting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CoverLetter",
+                table: "JobApplications",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CoverLetter",
+                table: "JobApplications",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+    }
+}
